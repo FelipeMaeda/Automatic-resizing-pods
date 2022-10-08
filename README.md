026e9a3da8637605c504c3c87d62fe898c71a0d9
Felipe Maeda <maedaxd@gmail.com>
2022-10-08 05:08:44 -0300

    Starting script to make report about consume in the Pods.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Automatic-scaling-pods
 Application that scaling automatically pods and deployments in the Kubernetes.
+
+```
+kind.exe create cluster --config .\cluster.yaml
+
+kubectl.exe apply -f .\enable_metrics_kind.yaml
+go mod tidy
+go run .\main.go
+```
+
+DECIMAL SI
+
+Memory:
+130792Ki    like    127Mi
+115Mi       like    115Mi
+
+CPU:
+192106837n  like    190m
+1000474931n like    1000m (1CPU)
--- main.go
+++ main.go
@@ -2,20 +2,59 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
+type podMetric struct {
+	Metadata struct {
+		Name      string `json:"name"`
+		Namespace string `json:"namespace"`
+	} `json:"metadata"`
+	Containers []struct {
+		Name  string `json:"name"`
+		Usage struct {
+			CPU    string `json:"cpu"`
+			Memory string `json:"memory"`
+		} `json:"usage"`
+	} `json:"containers"`
+}
+
 func main() {
-	// uses the current context in kubeconfig
-	// path-to-kubeconfig -- for example, /root/.kube/config
-	config, _ := clientcmd.BuildConfigFromFlags("", "<path-to-kubeconfig>")
-	// creates the clientset
+	config, _ := clientcmd.BuildConfigFromFlags("", "C:/Users/Rig/.kube/config")
 	clientset, _ := kubernetes.NewForConfig(config)
-	// access the API to list pods
 	pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	GetMetrics()
+}
+
+func GetMetrics() {
+	config, _ := clientcmd.BuildConfigFromFlags("", "C:/Users/Rig/.kube/config")
+
+	clientset, err := metricsv.NewForConfig(config)
+	if err != nil {
+		fmt.Printf(err.Error())
+		panic("ERRO 1")
+	}
+
+	for i := 0; i != -1; i++ {
+		data, err := clientset.RESTClient().Get().AbsPath("apis/metrics.k8s.io/v1beta1/namespaces/app/pods/nginx-deployment-69cb66965-jwfmf").DoRaw(context.TODO())
+		if err != nil {
+			fmt.Printf(err.Error())
+			panic("Error Collecting Metrics for the PODs.")
+		}
+
+		formated_data := podMetric{}
+		fmt.Println(string(data))
+		json.Unmarshal([]byte(string(data)), &formated_data)
+		fmt.Printf("Pod: %s Usage: %s\n", formated_data.Metadata, formated_data.Containers)
+		time.Sleep(1 * time.Second)
+	}
 }
